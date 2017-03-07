de3096799c83882259b370af9e35c04c4094c159
torresrl <torres.lande@gmail.com>
2017-03-07 14:37:42 +0100

    fant to bugs

--- program/src/Controller.java
+++ program/src/Controller.java
@@ -6,4 +6,12 @@ public class Controller {
     @FXML protected void handleAddButton (Event event){
 
         }
+
+    @FXML protected void handleWeeksBest(Event event){
+
+    }
+
+    @FXML protected void handleStatestikk(Event event){
+
+    }
 }
