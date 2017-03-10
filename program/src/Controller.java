7226ac657d6eac4489cd8e565e648ad3d798ce7d
Henrik Hind Sørensen <henrisor@stud.ntnu.no>
2017-03-10 17:49:48 +0100

    Lagde fxml for å registrere ny økt og mappet de i controlleren. 'ny økt' knappen åpner nå nyOkt.fxml. Foreløpig ingenting som hentes eller lagres i databasen

--- program/src/Controller.java
+++ program/src/Controller.java
@@ -1,11 +1,32 @@
+import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Controller {
 
-    @FXML protected void handleAddButton (Event event){
+    @FXML
+    protected Button addButton, WeeksBestButton, StatestikkButton;
 
+    @FXML protected void handleAddButton (ActionEvent event) {
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("nyOkt.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            stage.show();
+
+        } catch (IOException e) {
+            System.out.println("Kunne ikke åpne shitn");
         }
+
+    }
 
     @FXML protected void handleWeeksBest(Event event){
 
@@ -14,4 +35,5 @@ public class Controller {
     @FXML protected void handleStatestikk(Event event){
 
     }
+
 }
