9fa5fdece978a9c4041a36f320d02cbb77fa9881
torresrl <torres.lande@gmail.com>
2017-03-13 11:13:04 +0100

    test

--- program/src/database/workout.java
+++ program/src/database/workout.java
@@ -1,7 +1,27 @@
 package database;
 
+import java.sql.SQLException;
+import java.time.LocalDate;
+
 /**
  * Created by torresrl on 13/03/2017.
  */
 public class workout extends DBConnect{
+
+
+    public workout(){
+        super();
+    }
+
+
+    public void addOkt(LocalDate date, String time, int duration, String note, String personligForm, String prestasjon,
+                  Boolean inndors, int temp, String weather,String AirCondition){
+
+        try {
+            stmt = conn.createStatement();
+        } catch( SQLException se){
+            se.printStackTrace();
+        }
+
+    }
 }
